835e762ecd414505c1b85b33fa2fd469153e2c0e
Herob527 <szymon.wrzos@onet.pl>
2024-01-28 22:34:30 +0100

    Made some progress and added more tests

--- src/normalise_vector.rs
+++ src/normalise_vector.rs
@@ -8,18 +8,30 @@ pub fn normalise_vector(vector: Vec<Value>) -> Vec<u8> {
     let mut vector_copy: Vec<u8> = vector.clone().iter().map(|x| x.value).collect();
     while !is_normalised {
         let mut remember: u8 = 0;
+        vector_copy.reverse();
         is_normalised = true;
-        let reversed_version = vector_copy.iter_mut().rev();
-        for (index, entry) in reversed_version.enumerate() {
-            if entry >= &mut 10 {
-                remember = (f32::from(entry.to_owned()) / f32::from(10 as u8)).floor() as u8;
-                let remainder = entry.to_owned() % 10 as u8;
+        let reversed_version = vector_copy.iter();
+        let mut resulting_vector: Vec<u8> = Vec::new();
+        for entry in reversed_version {
+            let current_entry = entry.to_owned() + remember;
+            if current_entry >= 10 {
+                remember = (f32::from(current_entry) / f32::from(10 as u8)).floor() as u8;
+                let remainder = current_entry.to_owned() % 10 as u8;
                 is_normalised = false;
+                resulting_vector.insert(0, remainder);
+                continue;
             }
+            resulting_vector.insert(0, current_entry);
         }
-        if remember > 0 {}
+        if remember > 0 {
+            resulting_vector.insert(0, remember);
+            is_normalised = true;
+        }
+        dbg!(&resulting_vector);
+        if is_normalised {
+            return resulting_vector;
+        }
     }
-    dbg!(vector);
     return vec![];
 }
 
@@ -33,5 +45,46 @@ mod tests2 {
     fn test_normalise() {
         let vector = multiply_vectors(vec![1, 2, 1], vec![9]);
         debug_assert_eq!(normalise_vector(vector), vec![1, 0, 8, 9]);
+    }
+
+    #[test]
+    fn test_normalise_2() {
+        let vector = multiply_vectors(vec![1, 1], vec![9]);
+        debug_assert_eq!(normalise_vector(vector), vec![9, 9]);
+    }
+
+    #[test]
+    fn test_normalise_3() {
+        let vector = multiply_vectors(vec![1, 1], vec![1, 0]);
+        debug_assert_eq!(normalise_vector(vector), vec![1, 1, 0]);
+    }
+
+    #[test]
+    fn test_normalise_4() {
+        let vector = multiply_vectors(vec![1], vec![1, 0]);
+        debug_assert_eq!(normalise_vector(vector), vec![1, 0]);
+    }
+
+    #[test]
+    fn test_normalise_5() {
+        let vector = multiply_vectors(vec![2], vec![1, 0]);
+        debug_assert_eq!(normalise_vector(vector), vec![2, 0]);
+    }
+
+    #[test]
+    fn test_normalise_6() {
+        let vector = multiply_vectors(vec![2], vec![5, 0]);
+        debug_assert_eq!(normalise_vector(vector), vec![1, 0, 0]);
+    }
+    #[test]
+    fn test_normalise_7() {
+        let vector = multiply_vectors(vec![2], vec![2]);
+        debug_assert_eq!(normalise_vector(vector), vec![4]);
+    }
+
+    #[test]
+    fn test_normalise_8() {
+        let vector = multiply_vectors(vec![1, 4], vec![1, 2]);
+        debug_assert_eq!(normalise_vector(vector), vec![1, 6, 8]);
     }
 }
