10069f221e7e49c74fe4e61802bb0e8c4113a6d9
Herob527 <szymon.wrzos@onet.pl>
2024-01-28 15:49:56 +0100

    Working on loop iterating as long as vector is not normalised

--- src/normalise_vector.rs
+++ src/normalise_vector.rs
@@ -4,6 +4,21 @@ pub fn normalise_vector(vector: Vec<Value>) -> Vec<u8> {
     if vector.iter().all(|entry| entry.value < 10) {
         return vector.iter().map(|entry| entry.value).collect();
     };
+    let mut is_normalised = false;
+    let mut vector_copy: Vec<u8> = vector.clone().iter().map(|x| x.value).collect();
+    while !is_normalised {
+        let mut remember: u8 = 0;
+        is_normalised = true;
+        let reversed_version = vector_copy.iter_mut().rev();
+        for (index, entry) in reversed_version.enumerate() {
+            if entry >= &mut 10 {
+                remember = (f32::from(entry.to_owned()) / f32::from(10 as u8)).floor() as u8;
+                let remainder = entry.to_owned() % 10 as u8;
+                is_normalised = false;
+            }
+        }
+        if remember > 0 {}
+    }
     dbg!(vector);
     return vec![];
 }
