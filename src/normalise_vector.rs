d4a41ca1b4bb07893ead362b539516cd0aac3c9c
Herob527 <szymon.wrzos@onet.pl>
2024-01-29 23:03:56 +0100

    Working still

--- src/normalise_vector.rs
+++ src/normalise_vector.rs
@@ -5,32 +5,42 @@ pub fn normalise_vector(vector: Vec<Value>) -> Vec<u8> {
         return vector.iter().map(|entry| entry.value).collect();
     };
     let mut is_normalised = false;
-    let mut vector_copy: Vec<u8> = vector.clone().iter().map(|x| x.value).collect();
+    let mut vector_copy = vector.clone();
+    let mut index = 0;
     while !is_normalised {
+        dbg!(index);
         let mut remember: u8 = 0;
         vector_copy.reverse();
         is_normalised = true;
         let reversed_version = vector_copy.iter();
         let mut resulting_vector: Vec<u8> = Vec::new();
         for entry in reversed_version {
-            let current_entry = entry.to_owned() + remember;
+            let current_entry = entry.to_owned().value + remember;
             if current_entry >= 10 {
                 remember = (f32::from(current_entry) / f32::from(10 as u8)).floor() as u8;
                 let remainder = current_entry.to_owned() % 10 as u8;
                 is_normalised = false;
-                resulting_vector.insert(0, remainder);
+                resulting_vector.insert(entry.col_sum, remainder);
                 continue;
             }
-            resulting_vector.insert(0, current_entry);
+
+            let val_under_index = resulting_vector.get(entry.col_sum);
+            dbg!(entry.col_sum);
+            if val_under_index.is_none() {
+                resulting_vector.insert(entry.col_sum, current_entry);
+            } else {
+                resulting_vector[entry.col_sum] += val_under_index.unwrap().to_owned();
+            }
+            dbg!(&resulting_vector);
         }
         if remember > 0 {
             resulting_vector.insert(0, remember);
             is_normalised = true;
         }
-        dbg!(&resulting_vector);
         if is_normalised {
             return resulting_vector;
         }
+        index += 1;
     }
     return vec![];
 }
@@ -86,5 +96,11 @@ mod tests2 {
     fn test_normalise_8() {
         let vector = multiply_vectors(vec![1, 4], vec![1, 2]);
         debug_assert_eq!(normalise_vector(vector), vec![1, 6, 8]);
+    }
+
+    #[test]
+    fn test_normalise_9() {
+        let vector = multiply_vectors(vec![1, 2, 1], vec![1, 0]);
+        debug_assert_eq!(normalise_vector(vector), vec![1, 2, 1, 0]);
     }
 }
